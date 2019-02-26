6c2ac526391ec368533808ba9c986e4e21c55745
Bruno Campos Martinelli <martinelli4630@gmail.com>
2019-02-26 11:15:58 -0300

    build

--- dist/assiste-ai/main.js
+++ dist/assiste-ai/main.js
@@ -1,5 +1,16 @@
 (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"],{
 
+/***/ "./package.json":
+/*!**********************!*\
+  !*** ./package.json ***!
+  \**********************/
+/*! exports provided: name, version, scripts, private, dependencies, devDependencies, default */
+/***/ (function(module) {
+
+module.exports = {"name":"assiste-ai","version":"0.0.1","scripts":{"ng":"ng","start":"ng serve","build":"ng build","test":"ng test","lint":"ng lint","e2e":"ng e2e"},"private":true,"dependencies":{"@angular/animations":"^7.2.4","@angular/cdk":"^7.3.1","@angular/common":"~7.2.0","@angular/compiler":"~7.2.0","@angular/core":"~7.2.0","@angular/forms":"~7.2.0","@angular/material":"^7.3.1","@angular/platform-browser":"~7.2.0","@angular/platform-browser-dynamic":"~7.2.0","@angular/router":"~7.2.0","@ngrx/effects":"^7.2.0","@ngrx/entity":"^7.2.0","@ngrx/router-store":"^7.2.0","@ngrx/store":"^7.2.0","@ngrx/store-devtools":"^7.2.0","core-js":"^2.5.4","hammerjs":"^2.0.8","lodash":"^4.17.11","ngx-toastr":"^9.1.2","rxjs":"~6.3.3","tslib":"^1.9.0","zone.js":"~0.8.26"},"devDependencies":{"@angular-devkit/build-angular":"~0.13.0","@angular/cli":"~7.3.1","@angular/compiler-cli":"~7.2.0","@angular/language-service":"~7.2.0","@types/jasmine":"~2.8.8","@types/jasminewd2":"~2.0.3","@types/node":"^11.9.5","codelyzer":"~4.5.0","jasmine-core":"~2.99.1","jasmine-spec-reporter":"~4.2.1","json-server":"^0.14.2","karma":"~3.1.1","karma-chrome-launcher":"~2.2.0","karma-coverage-istanbul-reporter":"~2.0.1","karma-jasmine":"~1.1.2","karma-jasmine-html-reporter":"^0.2.2","ngrx-store-freeze":"^0.2.4","protractor":"~5.4.0","ts-node":"~7.0.0","tslint":"~5.11.0","typescript":"~3.2.2"}};
+
+/***/ }),
+
 /***/ "./src/$$_lazy_route_resource lazy recursive":
 /*!**********************************************************!*\
   !*** ./src/$$_lazy_route_resource lazy namespace object ***!
@@ -2690,7 +2701,8 @@ __webpack_require__.r(__webpack_exports__);
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 var environment = {
-    production: false
+    production: false,
+    version: __webpack_require__(/*! ../../package.json */ "./package.json").version
 };
 /*
  * For easier debugging in development mode, you can import the following file
